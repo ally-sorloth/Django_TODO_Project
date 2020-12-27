45ffbe21d0e851e70f0d5136c59e568d8d8febc7
ally-sorloth <allysorloth@gmail.com>
2020-12-27 21:11:10 +0300

    create models, todo list and forms

--- src/todo/views.py
+++ src/todo/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 
+from .forms import TodoAddForm
+from .models import Todo
+
+
 def home(request):
     return render(request, "todo/home.html")
 
-# Create your views here.
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos
+    }
+    return render(request, "todo/todo_list.html", context)
+
+
+def todo_create(request):
+    form = TodoAddForm()
+    context = {
+        'form': form
+    }
+    return render(request, "todo/todo_create.html", context)
